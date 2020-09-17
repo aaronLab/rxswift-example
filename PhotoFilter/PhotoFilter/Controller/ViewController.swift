57d639b2c8f8845d537b4c308d1600f039055505
Aaron Lee <aaronlab.net@gmail.com>
2020-09-17 21:03:06 +0900

    Observable Filter

--- PhotoFilter/PhotoFilter/Controller/ViewController.swift
+++ PhotoFilter/PhotoFilter/Controller/ViewController.swift
@@ -14,13 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var filterButton: UIButton!
     
     let disposeBag = DisposeBag()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         guard let navC = segue.destination as? UINavigationController,
@@ -37,10 +37,25 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func filterButtonPressed(_ sender: UIButton) {
+        
+        guard let sourceImage = self.photoImageView.image else { return }
+        
+        FilterService().applyFilter(to: sourceImage)
+            .subscribe(onNext: { filteredImage in
+                
+                DispatchQueue.main.async {
+                    self.photoImageView.image = filteredImage
+                }
+                
+            }).disposed(by: disposeBag)
+        
+    }
+    
     private func updateUI(with image: UIImage) {
         self.photoImageView.image = image
         self.filterButton.isHidden = false
     }
-
+    
 }
 
--- PhotoFilter/PhotoFilter/Helper/FilterService.swift
+++ PhotoFilter/PhotoFilter/Helper/FilterService.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreImage
+import RxSwift
 
 class FilterService {
     
@@ -16,10 +17,22 @@ class FilterService {
         self.context = CIContext()
     }
     
-    func applyFilter(to inputImage: UIImage, completion: @escaping ((UIImage) -> ())) {
+    func applyFilter(to inputImage: UIImage) -> Observable<UIImage> {
+        
+        return Observable<UIImage>.create { observer in
+            
+            self.applyFilter(to: inputImage) { (filteredImage) in
+                observer.onNext(filteredImage)
+            }
+            
+            return Disposables.create()
+        }
+    }
+    
+    private func applyFilter(to inputImage: UIImage, completion: @escaping ((UIImage) -> ())) {
         
         let filter = CIFilter(name: "CICMYKHalftone")!
-        filter.setValue(3.0, forKey: kCIInputWidthKey)
+        filter.setValue(1.0, forKey: kCIInputWidthKey)
         
         if let sourceImage = CIImage(image: inputImage) {
             
