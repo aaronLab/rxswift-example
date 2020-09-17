e67c67df7ecfc1b24bb391b0cd8f951cdf73209e
Aaron Lee <aaronlab.net@gmail.com>
2020-09-17 20:22:40 +0900

    setup: collection view

--- PhotoFilter/PhotoFilter/View Controller/PhotosCollectionViewController.swift
+++ PhotoFilter/PhotoFilter/View Controller/PhotosCollectionViewController.swift
@@ -6,11 +6,65 @@
 //
 
 import UIKit
+import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    private var images = [PHAsset]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.populatePhotos()
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else { return UICollectionViewCell() }
+        
+        let asset = self.images[indexPath.row]
+        let manager = PHImageManager.default()
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
+            
+            DispatchQueue.main.async {
+                cell.photoImageView.image = image
+            }
+            
+        }
+        
+        return cell
+    }
+    
+    private func populatePhotos() {
+        /*
+         Request Authorization for Photo Library
+         */
+        PHPhotoLibrary.requestAuthorization { [weak self] (status) in
+            
+            if status == .authorized {
+                // access the photos from photo library
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                
+                assets.enumerateObjects { (object, count, stop) in
+                    self?.images.append(object)
+                }
+                
+                self?.images.reverse()
+                
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
+            }
+            
+        }
+        
     }
     
 }
---
+++ PhotoFilter/PhotoFilter/View/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoFilter
+//
+//  Created by Aaron Lee on 9/17/20.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
