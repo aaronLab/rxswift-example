1a3de6e2a219afdc53bf4065538da745a0bbc92a
Aaron Lee <aaronlab.net@gmail.com>
2020-09-18 18:06:37 +0900

    RxSwift: Take While

--- HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -4,10 +4,10 @@ import RxSwift
 
 let disposeBag = DisposeBag()
 
-Observable.of(1, 2, 3, 4, 5, 6)
-    .take(3)
+Observable.of(2, 4, 6, 7, 8)
+    .takeWhile { $0 % 2 == 0}
     .subscribe(onNext: {
         print($0)
     }).disposed(by: disposeBag)
 
-// -> 1, 2, 3
+// -> 2, 3, 6
