f78a0e6768c281e25db4c0fe2e8d7e8734149031
Aaron Lee <aaronlee@Aarons-MacBook-Pro.local>
2020-10-11 19:29:57 +0900

    Camera Filter: Subscribing & Displaying Photo

--- Camera Filter/Camera Filter/PhotosCollectionViewController.swift
+++ Camera Filter/Camera Filter/PhotosCollectionViewController.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 import Photos
+import RxSwift
 
 class PhotosCollectionViewController: UICollectionViewController {
+
+    private let selectedPhotoSubject = PublishSubject<UIImage>()
+    var selectedPhoto: Observable<UIImage> {
+        return selectedPhotoSubject.asObservable()
+    }
 
     private var images = [PHAsset]()
 
@@ -31,7 +37,7 @@ class PhotosCollectionViewController: UICollectionViewController {
                 }
 
                 self?.images.reverse()
-                
+
                 DispatchQueue.main.async {
                     self?.collectionView.reloadData()
                 }
@@ -48,7 +54,7 @@ class PhotosCollectionViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.images.count
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? PhotoCollectionViewCell else { fatalError("Collection View Cell not Found") }
@@ -57,18 +63,37 @@ class PhotosCollectionViewController: UICollectionViewController {
         let manager = PHImageManager.default()
 
         manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
-            
+
             DispatchQueue.main.async {
                 cell.imgView.image = image
             }
-            
+
         }
 
         return cell
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         self.dismiss(animated: true, completion: nil)
+
+        let selectedAsset = self.images[indexPath.row]
+        PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, info in
+            
+            guard let info = info else { return }
+            
+            let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+            
+            if !isDegradedImage {
+                
+                if let image = image {
+                    self?.selectedPhotoSubject.onNext(image)
+                    self?.dismiss(animated: true, completion: nil)
+                }
+                
+            }
+            
+        }
+
     }
 
 }
--- Camera Filter/Camera Filter/ViewController.swift
+++ Camera Filter/Camera Filter/ViewController.swift
@@ -11,16 +11,37 @@ import RxSwift
 class ViewController: UIViewController {
 
     @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var applyFilterBtn: UIButton!
     
+    let disposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
-    
-    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navC = segue.destination as? UINavigationController,
+            let photosCVC = navC.viewControllers.first as? PhotosCollectionViewController
+            else {
+                fatalError("Segue Destination is not found")
+        }
+
+        photosCVC.selectedPhoto.subscribe(onNext: { [ weak self] photo in
+            
+            DispatchQueue.main.async {
+                self?.imgView.image = photo
+                self?.applyFilterBtn.isHidden = false
+            }
+
+        }).disposed(by: disposeBag)
+
+    }
+
+
     @IBAction func applyFilterPressed(_ sender: UIButton) {
     }
-    
+
 }
 
