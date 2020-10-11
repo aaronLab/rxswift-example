b4a14a082c343faf8befe2a5ac595968d9df4c03
Aaron Lee <aaronlee@Aarons-MacBook-Pro.local>
2020-10-11 17:58:03 +0900

    Camera Filter: Init UI

---
+++ Camera Filter/Camera Filter/PhotosCollectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosCollectionViewController.swift
+//  Camera Filter
+//
+//  Created by Aaron Lee on 2020-10-11.
+//
+
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
--- Camera Filter/Camera Filter/ViewController.swift
+++ Camera Filter/Camera Filter/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
+    
+    
+    @IBAction func applyFilterPressed(_ sender: UIButton) {
+    }
+    
 }
 
