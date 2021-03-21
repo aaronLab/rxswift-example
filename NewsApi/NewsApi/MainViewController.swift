d529fbc3361cd41b7ff3daa7a4e5cab62caa8e89
Aaron Lee <aaronlab.net@gmail.com>
2021-03-21 22:52:21 +0900

    init app

---
+++ NewsApi/NewsApi/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  NewsApi
+//
+//  Created by Aaron Lee on 2021/03/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Helpers
+    
+    func setupViews() {
+        
+    }
+
+}
+
