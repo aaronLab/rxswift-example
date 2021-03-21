72831015e6fd68db28da3cb26b4ef4ebb1184fa1
Aaron Lee <aaronlab.net@gmail.com>
2021-03-21 21:02:58 +0900

    subscribe

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -8,3 +8,17 @@ let observable2 = Observable.of(1, 2, 3)
 let observable3 = Observable.of([1, 2, 3])
 
 let observable4 = Observable.from([1, 2, 3, 4, 5])
+
+observable4.subscribe { event in
+    guard let element = event.element else { return }
+    print(element)
+}
+
+observable3.subscribe { event in
+    guard let element = event.element else { return }
+    print(element)
+}
+
+observable4.subscribe(onNext: { element in
+    print(element)
+})
