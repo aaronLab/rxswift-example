fa10c433fe8c7cbc319fb3e0085295c391104843
Aaron Lee <aaronlab.net@gmail.com>
2021-03-21 23:25:54 +0900

    fetching news

--- NewsApi/NewsApi/NewsTableViewController.swift
+++ NewsApi/NewsApi/NewsTableViewController.swift
@@ -6,26 +6,41 @@
 //
 
 import UIKit
+import RxSwift
 
 class NewsTableViewController: UITableViewController {
     
     // MARK: - Properties
+    
+    private let disposeBag = DisposeBag()
     
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        populateNews()
     }
     
     // MARK: - Actions
     
     // MARK: - Helpers
     
-    func setupViews() {
+    private func setupViews() {
         view.backgroundColor = .white
         title = "News App"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func populateNews() {
+        guard let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=\(APIKey)") else { return }
+        let resource = Resource<ArticleResponse>(url: url)
+        
+        URLRequest.load(resource: resource)
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
     }
 
 }
