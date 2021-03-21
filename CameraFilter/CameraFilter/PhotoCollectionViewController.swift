d7e0f973a700534106f9640308d49c8b455b3bea
Aaron Lee <aaronlab.net@gmail.com>
2021-03-21 21:53:03 +0900

    implement rxswift

---
+++ CameraFilter/CameraFilter/PhotoCollectionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewController.swift
+//  CameraFilter
+//
+//  Created by Aaron Lee on 2021/03/21.
+//
+
+import UIKit
+
+class PhotoCollectionViewController: UICollectionViewController {
+    
+}
---
+++ CameraFilter/CameraFilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Aaron Lee on 2021/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
+
