766fdbe3046ace1f976b654a3f72ff0b27c305be
Aaron Lee <aaronlab.net@gmail.com>
2021-03-21 21:18:04 +0900

    ReplaySubject

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -3,13 +3,23 @@ import RxSwift
 
 let disposeBag = DisposeBag()
 
-let subject = BehaviorSubject(value: "Initial Value")
-
-subject.onNext("Last Issue")
-
-subject.subscribe { event in
-    print(event)
-}
+let subject = ReplaySubject<String>.create(bufferSize: 2)
 
 subject.onNext("Issue 1")
+subject.onNext("Issue 2")
+subject.onNext("Issue 3")
 
+subject.subscribe {
+    // => 2, 3, 4, 5, 6
+    print($0)
+}
+
+subject.onNext("Issue 4")
+subject.onNext("Issue 5")
+subject.onNext("Issue 6")
+
+print("Subscription 2")
+subject.subscribe {
+    // => 5, 6
+    print($0)
+}
