d09af4be1dc4707fef162ea55aa166c2a5d05f87
Aaron Lee <aaronlab.net@gmail.com>
2021-03-22 21:40:49 +0900

    implement observables

---
+++ Observables/Observables/Observables.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+import RxSwift
+
+let observable1 = Observable.just(1)
+
+let observable2 = Observable.of(1, 2, 3)
+
+let observable3 = Observable.of([1, 2, 3])
+
+let observable4 = Observable.from([1, 2, 3, 4, 5])
