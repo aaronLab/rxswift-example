6b8d41d771a8458388a6154f1aef69f8be00229c
Aaron Lee <aaronlab.net@gmail.com>
2021-03-22 21:45:43 +0900

    subscription

--- Observables/Observables/Observables.playground/Contents.swift
+++ Observables/Observables/Observables.playground/Contents.swift
@@ -8,3 +8,29 @@ let observable2 = Observable.of(1, 2, 3)
 let observable3 = Observable.of([1, 2, 3])
 
 let observable4 = Observable.from([1, 2, 3, 4, 5])
+
+print("==============================")
+print("from...[1, 2, 3, 4, 5]")
+observable4.subscribe { event in
+    guard let element = event.element else { return }
+    // by each order
+    print(element)
+}
+print("==============================")
+
+
+
+print("of...[1, 2, 3]")
+observable3.subscribe { event in
+    guard let element = event.element else { return }
+    // at once
+    print(element)
+}
+print("==============================")
+
+print("onNext...")
+observable4.subscribe(onNext: { element in
+    // without unwrapping
+    print(element)
+})
+print("==============================")
