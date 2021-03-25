a48fbbf3283d1b89a02de4c611c69948765283e7
Aaron Lee <aaronlab.net@gmail.com>
2021-03-25 23:31:50 +0900

    fetch method

---
+++ Weather/Weather/Extensions/URLRequest+Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  URLRequest+Extensions.swift
+//  Weather
+//
+//  Created by Aaron Lee on 2021/03/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct Resource<T> {
+    let url: URL
+}
+
+extension URLRequest {
+    
+    static func load<T: Decodable>(resource: Resource<T>) -> Observable<T> {
+        
+        return Observable.from([resource.url])
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url)
+                
+                return URLSession.shared.rx.data(request: request)
+            }.map { data -> T in
+                return try JSONDecoder().decode(T.self, from: data)
+            }.asObservable()
+        
+    }
+    
+}
