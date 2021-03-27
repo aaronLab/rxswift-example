6a8580c6264633cbd8de5b57478dc086c27f9b3b
Aaron Lee <aaronlab.net@gmail.com>
2021-03-27 18:28:07 +0900

    project setup

---
+++ News/News/Views/Main/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  News
+//
+//  Created by Aaron Lee on 2021/03/27.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Helpers
+    
+    private func configureView() {
+        view.backgroundColor = .white
+        title = "News"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+}
