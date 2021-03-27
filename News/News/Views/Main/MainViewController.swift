5bed53c1b29397b9b24b108cf80a0b3d307b9667
Aaron Lee <aaronlab.net@gmail.com>
2021-03-27 18:39:51 +0900

    fetch news

--- News/News/Views/Main/MainViewController.swift
+++ News/News/Views/Main/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RxSwift
 
 class MainViewController: UITableViewController {
     
@@ -13,11 +14,14 @@ class MainViewController: UITableViewController {
     
     // MARK: - Properties
     
+    private let disposeBag = DisposeBag()
+    
     // MARK: - Lifecycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
+        populateNews()
     }
     
     // MARK: - Actions
@@ -28,6 +32,17 @@ class MainViewController: UITableViewController {
         view.backgroundColor = .white
         title = "News"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func populateNews() {
+        
+        let resource = Resource<ArticleResponse>(url: URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=\(APIKey)")!)
+        
+        URLRequest.load(resource: resource)
+            .subscribe(onNext: {
+                print($0)
+            }).disposed(by: disposeBag)
+        
     }
     
 }
