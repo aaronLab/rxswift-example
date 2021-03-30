5a57a21e39ad67f963c9572d0291a5c26caf9664
Aaron Lee <aaronlab.net@gmail.com>
2021-03-30 23:50:31 +0900

    Subscribing to observables

--- 02Observables/02Observables/02Observables/Playgrounds/Observable.playground/Contents.swift
+++ 02Observables/02Observables/02Observables/Playgrounds/Observable.playground/Contents.swift
@@ -9,3 +9,72 @@ let observable1 = Observable<Int>.just(one)
 let observable2 = Observable.of(one, two, three)
 let observable3 = Observable.of([one, two, three])
 let observable4 = Observable.from([one, two, three])
+
+observable2.subscribe { event in
+    if let element = event.element {
+        print(element)
+    }
+}
+
+print()
+
+observable2.subscribe(onNext: { element in
+    print(element)
+})
+
+print()
+
+observable4.subscribe { event in
+    print(event)
+}
+
+print()
+
+let observable5 = Observable<Void>.empty()
+
+observable5.subscribe(
+    onNext: { element in
+        print(element)
+    },
+    onCompleted:  {
+        print("Completed")
+    }
+)
+
+//print()
+//
+//let observable6 = Observable<Void>.never()
+//
+//observable6
+//    .do(onSubscribe: {
+//        print("Subscribed")
+//    })
+//    .subscribe(
+//        onNext: { element in
+//            print(element)
+//        },
+//        onCompleted: {
+//            print("Completed")
+//        },
+//        onDisposed: {
+//            print("Disposed")
+//        }
+//    )
+
+print()
+
+let observable7 = Observable<Int>.range(start: 1, count: 10)
+
+observable7
+    .subscribe(onNext: { i in
+        
+        let n = Double(i)
+        
+        let fibonacci = Int(
+            ((pow(1.61803, n) - pow(0.61803, n)) /
+                2.23606).rounded()
+        )
+        
+        print(fibonacci)
+        
+    })
