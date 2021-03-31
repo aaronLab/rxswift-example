e7333f6b4edfcc31fb588186bdfa714756868efb
Aaron Lee <aaronlab.net@gmail.com>
2021-03-31 19:08:21 +0900

    disposing

--- 02Observables/02Observables/02Observables/Playgrounds/Disposing+Terminating.playground/Contents.swift
+++ 02Observables/02Observables/02Observables/Playgrounds/Disposing+Terminating.playground/Contents.swift
@@ -1,6 +1,10 @@
 import Foundation
 import RxSwift
 
+enum MyError: Error {
+    case myError
+}
+
 let observable1 = Observable.of("A", "B", "C")
 
 let subscription = observable1.subscribe { event in
@@ -17,4 +21,22 @@ Observable.of("D", "E", "F")
     }
     .disposed(by: bag)
 
-
+Observable<String>.create { observer in
+    
+    observer.onNext("1")
+    
+    observer.onError(MyError.myError)
+    
+    observer.onCompleted()
+    
+    observer.onNext("?")
+    
+    return Disposables.create()
+    
+}.subscribe(
+    onNext: { print($0) },
+    onError: { print($0) },
+    onCompleted: { print("completed") },
+    onDisposed: { print("disposed") }
+)
+.disposed(by: bag) // => commented onError, conCompleted, and this => memory leak
