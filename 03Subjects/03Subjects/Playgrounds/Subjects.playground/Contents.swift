aaad1102ea72c3cfadefaf734f9e263e392f2f04
Aaron Lee <aaronlab.net@gmail.com>
2021-03-31 21:47:48 +0900

    PublishSubjects

--- 03Subjects/03Subjects/Playgrounds/Subjects.playground/Contents.swift
+++ 03Subjects/03Subjects/Playgrounds/Subjects.playground/Contents.swift
@@ -12,3 +12,32 @@ let subscriptionOne = subject
 
 subject.on(.next("1"))
 subject.onNext("2")
+
+let subscriptionTwo = subject
+    .subscribe { event in
+        guard let el = event.element else { return }
+        print("2) \(el)")
+    }
+
+subject.onNext("3")
+
+subscriptionOne.dispose()
+
+subject.onNext("4")
+
+subject.onCompleted()
+
+subject.onNext("5")
+
+subscriptionTwo.dispose()
+
+let bag = DisposeBag()
+
+subject
+    .subscribe {
+        guard let el = $0.element else { return }
+        print("3) \(el)")
+    }
+    .disposed(by: bag)
+
+subject.onNext("?")
