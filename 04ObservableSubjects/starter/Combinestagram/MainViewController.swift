d8d8360d5e92509491e2adba272b49a34d63d833
Aaron Lee <aaronlab.net@gmail.com>
2021-04-01 18:48:24 +0900

    adding photos

--- 04ObservableSubjects/starter/Combinestagram/MainViewController.swift
+++ 04ObservableSubjects/starter/Combinestagram/MainViewController.swift
@@ -35,29 +35,43 @@ import RxSwift
 import RxRelay
 
 class MainViewController: UIViewController {
-
+  
+  private let bag = DisposeBag()
+  private let images = BehaviorRelay<[UIImage]>(value: [])
+  
   @IBOutlet weak var imagePreview: UIImageView!
   @IBOutlet weak var buttonClear: UIButton!
   @IBOutlet weak var buttonSave: UIButton!
   @IBOutlet weak var itemAdd: UIBarButtonItem!
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    images.subscribe(onNext: { [weak imagePreview] photos in
+      
+      guard let preview = imagePreview else { return }
+      
+      DispatchQueue.main.async {
+        preview.image = photos.collage(size: preview.frame.size)
+      }
+      
+    })
+    .disposed(by: bag)
   }
   
   @IBAction func actionClear() {
-
+    images.accept([])
   }
-
+  
   @IBAction func actionSave() {
-
+    
   }
-
+  
   @IBAction func actionAdd() {
-
+    let newImages = images.value
+      + [UIImage(named: "IMG_1907.jpg")!]
+    images.accept(newImages)
   }
-
+  
   func showMessage(_ title: String, description: String? = nil) {
     let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
     alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
