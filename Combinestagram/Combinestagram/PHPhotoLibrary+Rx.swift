0082c4d7d0764b2d2de6cbe9b78f1f360d50f189
Aaron Lee <aaronlab.net@gmail.com>
2021-04-02 18:14:17 +0900

    extension

---
+++ Combinestagram/Combinestagram/PHPhotoLibrary+Rx.swift
@@ -0,0 +1,43 @@
+//
+//  PHPhotoLibrary+Rx.swift
+//  Combinestagram
+//
+//  Created by Aaron Lee on 2021-04-02.
+//  Copyright © 2021 Underplot ltd. All rights reserved.
+//
+
+import Foundation
+import Photos
+import RxSwift
+
+extension PHPhotoLibrary {
+  
+  static var authorized: Observable<Bool> {
+    return Observable.create { observer in
+      
+      DispatchQueue.main.async {
+        
+        if authorizationStatus() == .authorized {
+          
+          observer.onNext(true)
+          observer.onCompleted()
+          
+        } else {
+          
+          observer.onNext(false)
+          requestAuthorization { newStatus in
+            
+            observer.onNext(newStatus == .authorized)
+            observer.onCompleted()
+            
+          }
+          
+        }
+        
+      }
+      
+      return Disposables.create()
+    }
+  }
+  
+}
