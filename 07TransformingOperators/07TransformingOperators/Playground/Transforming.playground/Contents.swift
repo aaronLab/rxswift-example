3303f92eb7ebd0a29a2d45996fc0e44f30669d5b
Aaron Lee <aaronlab.net@gmail.com>
2021-04-02 22:33:58 +0900

    enumerated

--- 07TransformingOperators/07TransformingOperators/Playground/Transforming.playground/Contents.swift
+++ 07TransformingOperators/07TransformingOperators/Playground/Transforming.playground/Contents.swift
@@ -23,4 +23,16 @@ Observable<Int>.of(123, 4, 56)
         print($0)
     })
     .disposed(by: bag)
+
+print("enumerated")
+
+Observable.of(1, 2, 3, 4, 5, 6)
+    .enumerated()
+    .map { index, i in
+        index > 2 ? i * 2 : i
+    }
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: bag)
     
