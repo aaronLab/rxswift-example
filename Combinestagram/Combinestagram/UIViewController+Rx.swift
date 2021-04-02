b9fb58aaf03d1cf56874268911bd3c8075809314
Aaron Lee <aaronlab.net@gmail.com>
2021-04-02 18:14:09 +0900

    extension

---
+++ Combinestagram/Combinestagram/UIViewController+Rx.swift
@@ -0,0 +1,33 @@
+//
+//  UIViewController+Rx.swift
+//  Combinestagram
+//
+//  Created by Aaron Lee on 2021-04-02.
+//  Copyright © 2021 Underplot ltd. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+extension UIViewController {
+  
+  func alert(title: String, text: String?) -> Completable {
+    return Completable.create { [weak self] completable in
+      
+      let alertVC = UIAlertController(title: title, message: text, preferredStyle: .alert)
+      alertVC.addAction(UIAlertAction(title: "Close", style: .default, handler: { _ in
+        completable(.completed)
+      }))
+      
+      self?.present(alertVC, animated: true, completion: nil)
+      
+      return Disposables.create {
+        
+        self?.dismiss(animated: true, completion: nil)
+        
+      }
+      
+    }
+  }
+  
+}
