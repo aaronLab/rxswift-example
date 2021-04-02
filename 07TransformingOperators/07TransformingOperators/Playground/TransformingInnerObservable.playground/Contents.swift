5b1f375c3d43a981557924a803c6a354bc78a275
Aaron Lee <aaronlab.net@gmail.com>
2021-04-02 22:50:11 +0900

    flatMapLatest

--- 07TransformingOperators/07TransformingOperators/Playground/TransformingInnerObservable.playground/Contents.swift
+++ 07TransformingOperators/07TransformingOperators/Playground/TransformingInnerObservable.playground/Contents.swift
@@ -38,3 +38,27 @@ syeda.score.onNext(95)
 syeda.score.onNext(100)
 
 aaron.score.onNext(90)
+
+print("-----")
+
+let laura = Student(score: BehaviorSubject<Int>(value: 80))
+let charlotte = Student(score: BehaviorSubject<Int>(value: 90))
+
+let student2 = PublishSubject<Student>()
+
+student2
+    .flatMapLatest {
+        $0.score
+    }
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: bag)
+
+student2.onNext(laura) // <<
+laura.score.onNext(85) // <<
+student2.onNext(charlotte) // <<
+
+laura.score.onNext(95)
+charlotte.score.onNext(100) // <<
+laura.score.onNext(99)
