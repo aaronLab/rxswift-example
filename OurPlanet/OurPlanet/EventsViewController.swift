8964e4a440cdc51bd5e39b5c86e98e02f5e1a360
Aaron Lee <aaronlab.net@gmail.com>
2021-04-03 21:18:40 +0900

    events

--- OurPlanet/OurPlanet/EventsViewController.swift
+++ OurPlanet/OurPlanet/EventsViewController.swift
@@ -39,12 +39,22 @@ class EventsViewController: UIViewController, UITableViewDataSource {
   @IBOutlet var tableView: UITableView!
   @IBOutlet var slider: UISlider!
   @IBOutlet var daysLabel: UILabel!
+  
+  let events = BehaviorRelay<[EOEvent]>(value: [])
+  let disposeBag = DisposeBag()
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     tableView.rowHeight = UITableView.automaticDimension
     tableView.estimatedRowHeight = 60
+    
+    events
+      .asObservable()
+      .subscribe(onNext: { [weak self] _ in
+        self?.tableView.reloadData()
+      })
+      .disposed(by: disposeBag)
   }
 
   @IBAction func sliderAction(slider: UISlider) {
@@ -52,11 +62,13 @@ class EventsViewController: UIViewController, UITableViewDataSource {
 
   // MARK: UITableViewDataSource
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return events.value.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell") as! EventCell
+    let event = events.value[indexPath.row]
+    cell.configure(event: event)
     return cell
   }
 
