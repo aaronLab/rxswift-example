e003b6b14f84147773bff8d79df9192bf0a38145
Aaron Lee <aaronlab.net@gmail.com>
2021-04-04 15:39:28 +0900

    Add APIKey

--- Wundercast/Wundercast/Controllers/ApiController.swift
+++ Wundercast/Wundercast/Controllers/ApiController.swift
@@ -93,7 +93,7 @@ class ApiController {
 
   /// The api key to communicate with openweathermap.org
   /// Create you own on https://home.openweathermap.org/users/sign_up
-  private let apiKey = "<#Your Key#>"
+  private let apiKey = MyAPIKey
 
   /// API base URL
   let baseURL = URL(string: "http://api.openweathermap.org/data/2.5")!
