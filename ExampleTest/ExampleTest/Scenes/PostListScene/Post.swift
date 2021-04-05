002da4b7d878d1feff44dd15d79b9f7f07a86260
Aaron Lee <aaronlab.net@gmail.com>
2021-04-05 17:58:12 +0900

    add post

---
+++ ExampleTest/ExampleTest/Scenes/PostListScene/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  ExampleTest
+//
+//  Created by Aaron Lee on 2021-04-05.
+//
+
+import Foundation
+
+struct Post: Decodable {
+    
+    let userId: Int?
+    let id: Int?
+    let title: String?
+    let body: String?
+    
+}
