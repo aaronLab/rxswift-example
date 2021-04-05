1cd1f942e045a54ed88311246810f8c5849dc8da
Aaron Lee <aaronlab.net@gmail.com>
2021-04-05 19:22:14 +0900

    change viewmodel

--- ExampleTest/ExampleTest/Scenes/PostListScene/PostListViewModel.swift
+++ ExampleTest/ExampleTest/Scenes/PostListScene/PostListViewModel.swift
@@ -9,41 +9,64 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class PostListViewModel {
-
-    private let service = PostListService()
+class PostListViewModel: BaseService {
+    
     private let bag = DisposeBag()
     
     var isLoading = PublishSubject<Bool>()
     var postList = [PostViewModel]()
-    var error = PublishSubject<Error>()
-
+    var error = PublishSubject<APIError>()
+    
     func loadPostList() {
         isLoading.onNext(true)
         
-        service.getPostList()
-            .retry(3)
-            .subscribe(
-            onNext: { [weak self] list in
-                self?.postList = list.compactMap(PostViewModel.init)
-                self?.isLoading.onNext(false)
-            },
-            onError: { error in
-                self.error.onNext(error)
+        let maxAttempts = 4
+        
+        let request = Request<[Post]>(request: getRequest(path: .posts))
+        
+        URLRequest.load(request: request)
+            .retry { e in
+                
+                e.enumerated()
+                    .flatMap { attempt, error -> Observable<Int> in
+                        
+                        guard let error = error as? APIError else {
+                            return Observable.error(APIError.unknown)
+                        }
+                        
+                        if error == APIError.decodingError {
+                            return Observable.error(APIError.decodingError)
+                        }
+                        
+                        if attempt >= maxAttempts - 1 {
+                            return Observable.error(error)
+                        }
+                        return Observable<Int>.timer(.seconds(attempt + 1), scheduler: MainScheduler.instance)
+                    }
+                
             }
-        )
+            .subscribe(
+                onNext: { [weak self] list in
+                    self?.postList = list.compactMap(PostViewModel.init)
+                    self?.isLoading.onNext(false)
+                },
+                onError: { [weak self] error in
+                    guard let error = error as? APIError else { return }
+                    self?.error.onNext(error)
+                }
+            )
             .disposed(by: bag)
         
     }
-
+    
 }
 
 extension PostListViewModel {
-
+    
     func postAt(_ index: Int) -> PostViewModel {
         return postList[index]
     }
-
+    
 }
 
 struct PostViewModel {
