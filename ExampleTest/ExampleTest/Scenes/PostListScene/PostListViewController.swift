07d1d4064ac7f642c19113de52e6af71ddbd4dbe
Aaron Lee <aaronlab.net@gmail.com>
2021-04-05 15:28:42 +0900

    add list vc

---
+++ ExampleTest/ExampleTest/Scenes/PostListScene/PostListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PostListViewController.swift
+//  ExampleTest
+//
+//  Created by Aaron Lee on 2021-04-05.
+//
+
+import UIKit
+
+class PostListViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+    
+    private func configureView() {
+        
+        title = "Post List"
+        view.backgroundColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+    
+}
