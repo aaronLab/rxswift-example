8800068dda72c484e6e9d0c4d7c9babed3c5a656
Aaron Lee <aaronlab.net@gmail.com>
2021-04-05 13:54:37 +0900

    news extensions

--- NewsExample/NewsExample/Controllers/NewsTableViewController.swift
+++ NewsExample/NewsExample/Controllers/NewsTableViewController.swift
@@ -6,19 +6,35 @@
 //
 
 import UIKit
+import RxSwift
 
 class NewsTableViewController: UITableViewController {
+    
+    private let bag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureViews()
+        populateNews()
     }
     
     private func configureViews() {
         view.backgroundColor = .white
         navigationController?.navigationBar.prefersLargeTitles = true
         title = "News App"
+    }
+    
+    private func populateNews() {
+        
+        let resource = Resource<ArticleResponse>(url: URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=\(APIKEY)")!)
+        
+//        URLRequest.load(resource: resource)
+//            .subscribe(onNext: {
+//                print($0)
+//            })
+//            .disposed(by: bag)
+        
     }
 
 
